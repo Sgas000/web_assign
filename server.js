3373a1787655605df39aed5ad4300bed9bd8b7ae
Sgas000 <alshynkalyev@gmail.com>
2025-02-15 13:24:38 +0500

    update

--- server.js
+++ server.js
@@ -152,3 +152,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+const cors = require('cors');
+app.use(cors());
